394f52b5f248d8c2d23152e70b9720ae0fe97210
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-07-11 12:28:10 +0200

    Convert tworzeniePostaci to a static method

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ package Company;
 public class Main {
     public static void main(String[] args) {
         Menu.showMenu();
-        Postac postac = new Postac();
-        postac.tworzeniePostaci();
+        Postac postac = Postac.tworzeniePostaci();
     }
 }
--- Postac.java
+++ Postac.java
@@ -5,30 +5,31 @@ import java.util.Scanner;
 public class Postac {
     String nazwa;
     String klasa;
+    private AtrybutyPostaci atrybutyPostaci;
 
-    Postac() {
-    }
-
-    public Postac(String nazwa, String klasa) {
+    public Postac(String nazwa, String klasa, AtrybutyPostaci atrybutyPostaci) {
         this.nazwa = nazwa;
         this.klasa = klasa;
+        this.atrybutyPostaci = atrybutyPostaci;
     }
 
-    void tworzeniePostaci() {
+    public static Postac tworzeniePostaci() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Podaj nazwe postaci:");
-        nazwa = scanner.nextLine();
+        String nazwa = scanner.nextLine();
         System.out.println("Nazwa postaci to: " + nazwa);
         System.out.println();
         System.out.println("Wybierz klase: elf, wojownik, mag");
-        klasa = scanner.nextLine();
+        String klasa = scanner.nextLine();
 
         AtrybutyPostaci atrybutyPostaci = wybierzKlase(klasa);
         System.out.println(atrybutyPostaci);
+
+       return new Postac(nazwa, klasa, atrybutyPostaci);
     }
 
-    private AtrybutyPostaci wybierzKlase(String klasa) {
+    private static AtrybutyPostaci wybierzKlase(String klasa) {
         if (klasa.equals("elf")) {
             System.out.println("Wybrałeś elfa");
             return new Elf();
